dcece4bf45d33ebcaf9abcff8f24de3c7a2e78b9
tobyns-max <tobyns@gmail.com>
2025-06-24 08:48:53 +1000

    ?

--- venv/Scripts/activate.fish
+++ venv/Scripts/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV 'C:\Users\TobyNorton-Smith\BidSmart\venv'
+set -gx VIRTUAL_ENV 'C:\Users\TobyNorton-Smith\BidSmart_parent\BidSmart\venv'
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"Scripts $PATH
